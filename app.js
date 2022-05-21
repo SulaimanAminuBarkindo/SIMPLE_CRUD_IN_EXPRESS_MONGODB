b60b838862fa34f9b049904e37c724e800bf777e
sulaiman <sulaimanaminu02@gmail.com>
2022-05-21 21:17:40 +0100

    create express server and connect to  mongodb

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cluster = 'cluster0'
+const dbName = 'BooksManagementDB'
+const dbUsername = 'souleyMongoDB'
+const dbPassword = 'Drsulaiman35'
+const dbURI = `mongodb+srv://${dbUsername}:${dbPassword}@${cluster}.zjd9u.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const port = '3000'
+
+app.use(express.json())
+
+mongoose.connect(dbURI, (error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('mongodb connected successfully')
+
+    // start server after database connection is established
+    app.listen(port, () => {
+      console.log(`server started on port ${port}`)
+    })
+  }
+})
